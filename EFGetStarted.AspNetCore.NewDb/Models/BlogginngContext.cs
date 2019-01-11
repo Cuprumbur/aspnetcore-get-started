c00507147f06d35a444947933e1412cf5977f207
darmaev <daramev@rbsoft.ru>
2019-01-11 23:55:37 +0800

    done

---
+++ EFGetStarted.AspNetCore.NewDb/Models/BlogginngContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace EFGetStarted.AspNetCore.NewDb.Models
+{
+    public class BlogginngContext : DbContext
+    {
+        public BlogginngContext(DbContextOptions<BlogginngContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+    }
+
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public ICollection<Post> Posts { get; set; }
+        public string Url { get; set; }
+    }
+
+    public class Post
+    {
+        public Blog Blog { get; set; }
+        public int BlogId { get; set; }
+        public string Content { get; set; }
+        public int PostId { get; set; }
+        public string Title { get; set; }
+    }
+}
